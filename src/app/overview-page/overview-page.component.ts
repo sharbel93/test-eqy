fe94ed5c1cd310446db6cb1f4fa4c3448754171b
sharbel93 <sharbel@techfactory.ltd>
2020-02-24 09:46:49 +0300

    updating ui design in overview page

--- src/app/overview-page/overview-page.component.ts
+++ src/app/overview-page/overview-page.component.ts
@@ -1,15 +1,68 @@
-import { Component, OnInit } from '@angular/core';
-
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {FormControl} from '@angular/forms';
+import { ChartDataSets, ChartOptions } from 'chart.js';
+import { Color, BaseChartDirective, Label } from 'ng2-charts';
+import * as pluginAnnotations from 'chartjs-plugin-annotation';
 @Component({
   selector: 'app-overview-page',
   templateUrl: './overview-page.component.html',
   styleUrls: ['./overview-page.component.css']
 })
 export class OverviewPageComponent implements OnInit {
+  Currencies: any[];
+  StateDisplay: any[];
+  selected: any;
+  selectedState: any;
+  public lineChartData: ChartDataSets[] = [
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' }
+  ];
+  public lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public lineChartOptions: (ChartOptions) = {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      xAxes: [{
+        gridLines: {
+          display: false
+        }
+      }]
+    }
+  };
+  public lineChartColors: Color[] = [
+    {
+      backgroundColor: 'rgba(0,0,0,0.0)',
+      borderColor: 'rgba(105, 174, 255, 0.47)',
+      pointBackgroundColor: 'rgba(0,0,0,0.0)',
+      pointBorderColor: '#603b80',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)',
+      pointRadius: 5,
+      pointBorderWidth: 2
+    }
+  ];
+  // #603b80
+  public lineChartLegend = false;
+  public lineChartType = 'line';
+  public lineChartPlugins = [pluginAnnotations];
 
+  @ViewChild(BaseChartDirective, { static: true }) chart: BaseChartDirective;
   constructor() { }
-
   ngOnInit(): void {
+    this.Currencies = [
+      { id: 1, name: 'KES'},
+      { id: 2, name: 'USD'},
+      { id: 3, name: 'EUR'},
+    ];
+    this.StateDisplay = [
+      { id: 1, name: 'Total Volume'},
+      { id: 2, name: 'Total Shares'},
+      { id: 3, name: 'Total Bond'},
+    ];
+
+    this.selected = new FormControl(1);
+    this.selectedState = new FormControl('Total Volume');
+    console.log(this.selectedState);
   }
+
 
 }
--- src/app/overview-page/overview.module.ts
+++ src/app/overview-page/overview.module.ts
@@ -3,7 +3,8 @@ import { CommonModule } from '@angular/common';
 import {OverviewPageComponent} from './overview-page.component';
 import {OverviewRoutingModule} from './overview-routing.module';
 import {AppMaterialModule} from '../app-material/app-material.module';
-
+import {ReactiveFormsModule} from '@angular/forms';
+import { ChartsModule } from 'ng2-charts';
 
 
 @NgModule({
@@ -13,7 +14,9 @@ import {AppMaterialModule} from '../app-material/app-material.module';
   imports: [
     CommonModule,
     OverviewRoutingModule,
-    AppMaterialModule
+    AppMaterialModule,
+    ReactiveFormsModule,
+    ChartsModule
   ]
 })
 export class OverviewModule { }
